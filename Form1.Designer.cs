4c17a09111033ee81c799d66d5db88ba436ddee4
Williamsgang <gb.williams02@yahoo.com>
2024-03-07 11:07:23 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace TestWinForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileOuput = new System.Windows.Forms.TextBox();
+            this.cmdOuput = new System.Windows.Forms.TextBox();
+            this.fileTree = new System.Windows.Forms.TreeView();
+            this.treeViewBtn = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.serverFiles = new System.Windows.Forms.TabPage();
+            this.createFileBtn = new System.Windows.Forms.Button();
+            this.saveFileBtn = new System.Windows.Forms.Button();
+            this.cmdOutput = new System.Windows.Forms.TabPage();
+            this.cmdInput = new System.Windows.Forms.Button();
+            this.cmdInputText = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.serverStartBtn = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.serverFiles.SuspendLayout();
+            this.cmdOutput.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileOuput
+            // 
+            this.fileOuput.AcceptsTab = true;
+            this.fileOuput.HideSelection = false;
+            this.fileOuput.Location = new System.Drawing.Point(8, 7);
+            this.fileOuput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.fileOuput.Multiline = true;
+            this.fileOuput.Name = "fileOuput";
+            this.fileOuput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.fileOuput.Size = new System.Drawing.Size(1172, 853);
+            this.fileOuput.TabIndex = 1;
+            this.fileOuput.TextChanged += new System.EventHandler(this.fileOuput_TextChanged);
+            // 
+            // cmdOuput
+            // 
+            this.cmdOuput.Location = new System.Drawing.Point(8, 7);
+            this.cmdOuput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdOuput.Multiline = true;
+            this.cmdOuput.Name = "cmdOuput";
+            this.cmdOuput.ReadOnly = true;
+            this.cmdOuput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.cmdOuput.Size = new System.Drawing.Size(1172, 854);
+            this.cmdOuput.TabIndex = 0;
+            // 
+            // fileTree
+            // 
+            this.fileTree.Location = new System.Drawing.Point(4, 4);
+            this.fileTree.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.fileTree.Name = "fileTree";
+            this.fileTree.Size = new System.Drawing.Size(364, 933);
+            this.fileTree.TabIndex = 2;
+            this.fileTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.fileTree_AfterSelect);
+            // 
+            // treeViewBtn
+            // 
+            this.treeViewBtn.Location = new System.Drawing.Point(4, 946);
+            this.treeViewBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.treeViewBtn.Name = "treeViewBtn";
+            this.treeViewBtn.Size = new System.Drawing.Size(100, 28);
+            this.treeViewBtn.TabIndex = 3;
+            this.treeViewBtn.Text = "TreeView";
+            this.treeViewBtn.UseVisualStyleBackColor = true;
+            this.treeViewBtn.Click += new System.EventHandler(this.treeViewBtn_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.serverFiles);
+            this.tabControl1.Controls.Add(this.cmdOutput);
+            this.tabControl1.Location = new System.Drawing.Point(378, 4);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1200, 934);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // serverFiles
+            // 
+            this.serverFiles.Controls.Add(this.createFileBtn);
+            this.serverFiles.Controls.Add(this.saveFileBtn);
+            this.serverFiles.Controls.Add(this.fileOuput);
+            this.serverFiles.Location = new System.Drawing.Point(4, 25);
+            this.serverFiles.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.serverFiles.Name = "serverFiles";
+            this.serverFiles.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.serverFiles.Size = new System.Drawing.Size(1192, 905);
+            this.serverFiles.TabIndex = 0;
+            this.serverFiles.Text = "Server Files";
+            this.serverFiles.UseVisualStyleBackColor = true;
+            // 
+            // createFileBtn
+            // 
+            this.createFileBtn.Location = new System.Drawing.Point(9, 866);
+            this.createFileBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.createFileBtn.Name = "createFileBtn";
+            this.createFileBtn.Size = new System.Drawing.Size(100, 28);
+            this.createFileBtn.TabIndex = 3;
+            this.createFileBtn.Text = "Create File";
+            this.createFileBtn.UseVisualStyleBackColor = true;
+            this.createFileBtn.Click += new System.EventHandler(this.createFileBtn_Click);
+            // 
+            // saveFileBtn
+            // 
+            this.saveFileBtn.AutoSize = true;
+            this.saveFileBtn.Location = new System.Drawing.Point(116, 866);
+            this.saveFileBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.saveFileBtn.Name = "saveFileBtn";
+            this.saveFileBtn.Size = new System.Drawing.Size(99, 32);
+            this.saveFileBtn.TabIndex = 2;
+            this.saveFileBtn.Text = "Save File";
+            this.saveFileBtn.UseVisualStyleBackColor = true;
+            this.saveFileBtn.Click += new System.EventHandler(this.saveFileBtn_Click);
+            // 
+            // cmdOutput
+            // 
+            this.cmdOutput.Controls.Add(this.cmdOuput);
+            this.cmdOutput.Controls.Add(this.cmdInput);
+            this.cmdOutput.Controls.Add(this.cmdInputText);
+            this.cmdOutput.Location = new System.Drawing.Point(4, 25);
+            this.cmdOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdOutput.Name = "cmdOutput";
+            this.cmdOutput.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdOutput.Size = new System.Drawing.Size(1192, 905);
+            this.cmdOutput.TabIndex = 1;
+            this.cmdOutput.Text = "Cmd Output";
+            this.cmdOutput.UseVisualStyleBackColor = true;
+            // 
+            // cmdInput
+            // 
+            this.cmdInput.Location = new System.Drawing.Point(1081, 869);
+            this.cmdInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdInput.Name = "cmdInput";
+            this.cmdInput.Size = new System.Drawing.Size(100, 25);
+            this.cmdInput.TabIndex = 2;
+            this.cmdInput.Text = "CMD";
+            this.cmdInput.UseVisualStyleBackColor = true;
+            this.cmdInput.Click += new System.EventHandler(this.cmdInput_Click);
+            // 
+            // cmdInputText
+            // 
+            this.cmdInputText.Location = new System.Drawing.Point(8, 870);
+            this.cmdInputText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdInputText.Name = "cmdInputText";
+            this.cmdInputText.Size = new System.Drawing.Size(1064, 22);
+            this.cmdInputText.TabIndex = 1;
+            this.cmdInputText.TextChanged += new System.EventHandler(this.cmdInputText_TextChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.66231F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.33769F));
+            this.tableLayoutPanel1.Controls.Add(this.tabControl1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.treeViewBtn, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.fileTree, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.serverStartBtn, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95.02763F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.972376F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1583, 992);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // serverStartBtn
+            // 
+            this.serverStartBtn.Location = new System.Drawing.Point(378, 946);
+            this.serverStartBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.serverStartBtn.Name = "serverStartBtn";
+            this.serverStartBtn.Size = new System.Drawing.Size(100, 28);
+            this.serverStartBtn.TabIndex = 5;
+            this.serverStartBtn.Text = "Server Start";
+            this.serverStartBtn.UseVisualStyleBackColor = true;
+            this.serverStartBtn.Click += new System.EventHandler(this.serverStartBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1605, 1009);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "Minecraft Server App";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.serverFiles.ResumeLayout(false);
+            this.serverFiles.PerformLayout();
+            this.cmdOutput.ResumeLayout(false);
+            this.cmdOutput.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        // Form1
+        private System.Windows.Forms.TextBox fileOuput;
+        private System.Windows.Forms.TreeView fileTree;
+        private System.Windows.Forms.Button treeViewBtn;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage serverFiles;
+        private System.Windows.Forms.TabPage cmdOutput;
+        private System.Windows.Forms.TextBox cmdOuput;
+        private System.Windows.Forms.Button cmdInput;
+        private System.Windows.Forms.TextBox cmdInputText;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button saveFileBtn;
+        private System.Windows.Forms.Button serverStartBtn;
+        private System.Windows.Forms.Button createFileBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestWinForm
+{
+    public partial class MainForm : Form
+    {
+        private string filePath;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SetFilePath(string path)
+        {
+            this.filePath = path;
+        }
+
+        private void treeViewBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() != DialogResult.OK) { return; }
+
+            fileTree.Nodes.Clear();
+
+            var node = TraverseDirectory(dialog.SelectedPath);
+            SetFilePath(dialog.SelectedPath);
+            node.Text = Path.GetFileName(node.Text);
+
+            fileTree.Nodes.Add(node);
+
+            node.Expand();
+        }
+
+        private TreeNode TraverseDirectory(string path)
+        {
+            TreeNode result = new TreeNode(path);
+
+            foreach (var subdirectory in Directory.GetDirectories(path))
+            {
+                var node = TraverseDirectory(subdirectory);
+                node.Text = Path.GetFileName(node.Text);
+                result.Nodes.Add(node);
+            }
+
+            foreach (var subfile in Directory.GetFiles(path))
+            {
+                var node = Path.GetFileName(subfile);
+                result.Nodes.Add(node);
+            }
+
+            return result;
+        }
+
+        private string serverFile;
+
+        private void fileTree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode treeNode = fileTree.SelectedNode;
+            string fullPath = filePath + @"\" + Path.GetFileName(treeNode.FullPath);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            string[] allowedExtensions = { ".txt", ".properties", ".log", ".bat", ".log.gz", ".json" };
+            string fileExtension = Path.GetExtension(fullPath);
+
+            switch ((e.Action))
+            {
+                case TreeViewAction.ByKeyboard:
+                    if (File.Exists(fullPath))
+                    {
+                        if (allowedExtensions.Any(ext => string.Equals(fileExtension, ext, StringComparison.OrdinalIgnoreCase)))
+                        {
+                            using (StreamReader file = new StreamReader(fullPath))
+                            {
+                                while (!file.EndOfStream)
+                                {
+                                    stringBuilder.Append(file.ReadLine());
+                                }
+                                fileOuput.Text = stringBuilder.ToString();
+                            }
+                            fileOuput.Focus();
+                        }
+                        else if (string.Equals(fileExtension, ".jar", StringComparison.OrdinalIgnoreCase))
+                        {
+                            serverFile = fullPath;
+                            fileOuput.Text += serverFile;
+                        }
+                        else
+                        {
+                            fileOuput.Text = "Invalid file extension. Expected: "
+                                + allowedExtensions[0] + ", "
+                                + allowedExtensions[1] + ", "
+                                + allowedExtensions[2] + ", "
+                                + allowedExtensions[3] + ", "
+                                + allowedExtensions[4] + ", "
+                                + allowedExtensions[5] + ".";
+                            fileOuput.Focus();
+                        }
+                    }
+                    else
+                    {
+                        fileOuput.Text = "File does not exist";
+                        fileOuput.Focus();
+                    }
+                    break;
+                case TreeViewAction.ByMouse:
+                    if (File.Exists(fullPath))
+                    {
+                        if (allowedExtensions.Any(ext => string.Equals(fileExtension, ext, StringComparison.OrdinalIgnoreCase)))
+                        {
+                            using (StreamReader file = new StreamReader(fullPath))
+                            {
+                                while (!file.EndOfStream)
+                                {
+                                    stringBuilder.Append(file.ReadLine());
+                                    stringBuilder.Append("\n");
+                                }
+                                fileOuput.Text = stringBuilder.ToString();
+                            }
+                            fileOuput.Focus();
+                        }
+                        else if (string.Equals(fileExtension, ".jar", StringComparison.OrdinalIgnoreCase))
+                        {
+                            serverFile = fullPath;
+                            fileOuput.Text += serverFile;
+                        }
+                        else
+                        {
+                            fileOuput.Text = "Invalid file extension. Expected: "
+                                + allowedExtensions[0] + ", "
+                                + allowedExtensions[1] + ", "
+                                + allowedExtensions[2] + ", "
+                                + allowedExtensions[3] + ", "
+                                + allowedExtensions[4] + ", "
+                                + allowedExtensions[5] + ".";
+                            fileOuput.Focus();
+                        }
+                    }
+                    else
+                    {
+                        fileOuput.Text = "File does not exist";
+                        fileOuput.Focus();
+                    }
+                    break;
+            }
+
+        }
+
+        private Process javaProcess = new Process();
+
+        private void cmdInput_Click(object sender, EventArgs e)
+        {
+
+            using (StreamWriter javaStreamWriter = javaProcess.StandardInput)
+            using (StreamReader javaStreamReader = javaProcess.StandardOutput)
+            {
+                string input = cmdInputText.Text;
+                javaStreamWriter.WriteLine(input);
+
+                string output = javaStreamReader.ReadLine();
+                cmdOuput.Text = output;
+            }
+        }
+
+        private void serverStartBtn_Click(object sender, EventArgs e)
+        {
+
+            javaProcess.StartInfo.FileName = "";
+            javaProcess.StartInfo.Arguments = ""; // java args
+            javaProcess.StartInfo.UseShellExecute = false;
+            javaProcess.StartInfo.RedirectStandardInput = true;
+            javaProcess.StartInfo.RedirectStandardOutput = true;
+            javaProcess.StartInfo.CreateNoWindow = false;
+
+            javaProcess.Start();
+
+            using (StreamWriter javaStreamWriter = javaProcess.StandardInput)
+            using (StreamReader javaStreamReader = javaProcess.StandardOutput)
+            {
+                string output = javaStreamReader.ReadLine();
+                cmdOuput.Text = output;
+            }
+
+            javaProcess.WaitForExit();
+        }
+
+        private void cmdInputText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileOuput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createFileBtn_Click(object sender, EventArgs e)
+        {
+            Form createFileForm = new CreateFileForm();
+            createFileForm.ShowDialog();
+
+
+
+        }
+
+        private void saveFileBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string defaultEnvVar = nameof(defaultEnvVar);
+            string processEnvVar = nameof(processEnvVar);
+            string userEnvVar = nameof(userEnvVar);
+            string machineEnvVar = nameof(machineEnvVar);
+
+            string dft = nameof(dft);
+            string process = nameof(process);
+            string user = nameof(user);
+            string machine = nameof(machine);
+
+
+        }
+    }
+
+    public partial class CreateFileForm : Form
+    {
+        private string file;
+
+        private string fileName;
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+
+        private string[] fileExtensions = { ".txt", ".properties", ".bat" };
+
+        private string fileExtension;
+        public string FileExtension
+        {
+            get { return fileExtension; }
+            set { fileExtension = value; }
+        }
+
+        private TextBox fileNameTextBox;
+        private Button saveFileInfo;
+        private Button closeFormBtn;
+        private RadioButton textFile;
+        private RadioButton propertiesFile;
+        private RadioButton batFile;
+
+        public CreateFileForm()
+        {
+            this.saveFileInfo = new Button();
+            this.textFile = new RadioButton();
+            this.batFile = new RadioButton();
+            this.propertiesFile = new RadioButton();
+            this.fileNameTextBox = new TextBox();
+
+            this.textFile.Enabled = true;
+            this.textFile.Text = ".txt";
+            this.textFile.Size = new System.Drawing.Size(50, 20);
+            this.textFile.Location = new System.Drawing.Point(20, 20);
+            this.Controls.Add(textFile);
+
+            this.batFile.Enabled = true;
+            this.batFile.Text = ".bat";
+            this.batFile.Size = new System.Drawing.Size(50, 20);
+            this.batFile.Location = new System.Drawing.Point(20, 40);
+            this.Controls.Add(batFile);
+
+            this.propertiesFile.Enabled = true;
+            this.propertiesFile.Text = ".properties";
+            this.propertiesFile.Size = new System.Drawing.Size(100, 20);
+            this.propertiesFile.Location = new System.Drawing.Point(20, 60);
+            this.Controls.Add(propertiesFile);
+
+            this.fileNameTextBox.Enabled = true;
+            this.fileNameTextBox.Size = new System.Drawing.Size(Width, Height);
+            this.fileNameTextBox.Location = new System.Drawing.Point(20, 80);
+            this.Controls.Add(fileNameTextBox);
+
+            this.saveFileInfo.Enabled = true;
+            this.saveFileInfo.Text = "Save File";
+            this.Location = new System.Drawing.Point(20, 100);
+            this.saveFileInfo.Click += new System.EventHandler(this.saveFileInfo_Click);
+            this.Controls.Add(saveFileInfo);
+
+            this.closeFormBtn.Enabled = true;
+            this.Text = "Close Form";
+            this.Location = new System.Drawing.Point(100, 100);
+
+
+            this.Text = "Hello World";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 360);
+        }
+
+        private void saveFileInfo_Click(object sender, EventArgs e)
+        {
+            string tempFileName = this.FileName;
+            string fileExtension = this.FileExtension;
+
+            string fileName = tempFileName + fileExtension;
+
+            this.file = fileName;
+            Console.WriteLine("SaveFileInfo Btn has been clicked.");
+
+        }
+
+        private void fileNameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            this.FileName = fileNameTextBox.Text;
+            Console.WriteLine("fileNameTextBox has been updated.");
+        }
+
+        private void textFile_CheckChanged(object sender, EventArgs e)
+        {
+            this.FileExtension = fileExtensions[0];
+            Console.WriteLine("textFile radiobutton has been updated.");
+        }
+
+        private void propertiesFile_CheckChanged(object sender, EventArgs e)
+        {
+            this.FileExtension = fileExtensions[1];
+            Console.WriteLine("propertiesFile radiobutton has been updated.");
+        }
+
+        private void batFile_CheckChanged(object sender, EventArgs e)
+        {
+            this.FileExtension = fileExtensions[2];
+            Console.WriteLine("batFile radiobutton has been updated.");
+        }
+    }
+}
